eaf8884c46e7e4404a29fb698bd3df92747a612e
Liam Graham <liam.david.graham@gmail.com>
2018-06-30 23:57:31 +1000

    Implement file versions retrieval

--- src/manager.py
+++ src/manager.py
@@ -34,11 +34,21 @@ except ImportError:
     sh = Sh()
 
 import time
+import os
 
 
 class FileManager:
-    def __init__(self):
-        pass
+    def __init__(self, repo_path):
+        """
+        Creates new FileManager for repository at given path.
+
+        Arguments:
+            repo_path (str): path of target repository
+        """
+        self.repo = sh.git.bake(
+            _cwd=repo_path
+        )
+        print(self.repo.log("--oneline", "file.txt"))
 
     def commit_changes(self):
         """
@@ -53,10 +63,10 @@ class FileManager:
             try:
                 codes = change[0]
                 file_name = change[1]
-                sh.git.add(file_name)
+                self.repo.add(file_name)
                 actions = " and ".join([verbose_codes[x] for x in codes]).capitalize()
                 message = f"{actions} {file_name}"
-                sh.git.commit(m=message)
+                self.repo.commit(m=message)
                 committed.append(file_name)
             except Exception:
                 continue
@@ -82,7 +92,7 @@ class FileManager:
         deleted, and MD for modified then deleted)
         """
         changes = []
-        status = sh.git.status("-s").split("\n")
+        status = self.repo.status("-s").split("\n")
         for line in status:
             if not line:
                 continue
@@ -91,11 +101,62 @@ class FileManager:
             changes.append((codes, line[1]))
         return changes
 
-    def view_file_version(self):
-        pass
+    def get_file_versions(self, file_name):
+        """
+        Returns all versions of given file, in order of most recent to least recent
+        (even if file has been renamed). The version list consists of pairs of
+        corresponding commit hashes and commit messages, in the form:
+
+        [(commit0, message0), (commit1, message1), ..., (commitN, messageN)]
+
+        Arguments:
+            file_name (str): name of file for which versions will be retrieved
+
+        Returns (list(str)): all versions of given file
+        """
+        file_log = self.repo.log("--follow", "--oneline", "test.docx").split("\n")
+        versions = []
+        for commit in file_log:
+            if not commit:
+                continue
+            commit = commit.split()
+            versions.append((commit[0], " ".join(commit[1:])))
+        return versions
+
+    def view_file_version(self, file_name, version_num):
+        """
+        Open given version of given file. 
+        
+        Arguments:
+            file_name (str): name of file
+            version_num (int): number of version to be retrieved
+
+        Notes:
+        View commit history of specific file: git log --oneline <file>
+
+        Get commit date of specific commit: git show --pretty=format:"%cd" --no-patch <hash>
+
+        1. View prior version of specific file - git checkout <hash> <file>
+        2. Return to current version - git checkout -- <file>
+        """
+        versions = self.get_file_versions(file_name)
+        if version_num >= len(versions):
+            return
+        target_ver = versions[version_num - 1]
+        self.repo.checkout(target_ver[0], file_name)
+        os.startfile(file_name)
+        self.repo.checkout("--", file_name)
 
     def revert_file_version(self):
+        """
+        1. View prior version of specific file - git checkout <hash> <file>
+        2. Commit changes
+
+        Retains commit history. 
+        """
         pass
 
-m = FileManager()
-print(m.get_changes())
+
+m = FileManager("C:\\Users\\Liam\\Google Drive\\Projects\\Small\\test-repo")
+print(m.get_file_versions("test.docx"))
+m.view_file_version("file.txt", 2)

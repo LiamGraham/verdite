b8835ba63ea863d6acaed7608251801f47b18ce0
Liam Graham <liam.david.graham@gmail.com>
2018-07-01 13:59:11 +1000

    Refactor control loop

--- src/control.py
+++ src/control.py
@@ -1,10 +1,12 @@
 import manage
-
-manager = manage.FileManager("C:\\Users\\Liam\\Google Drive\\Projects\\Small\\test-repo")
+import time
 
 def control_loop():
+    manager = manage.FileManager("C:\\Users\\Liam\\Google Drive\\Projects\\Small\\test-repo")
     while True:
+        time.sleep(5)
         if not manager.has_changed():
+            print("No changes")
             continue
         print(manager.store_changes())
 
